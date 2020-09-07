6d09c2276fca000c852b0c0c0540b16bddea27a9
Mo Leo Sam <leo89_my@hotmail.com>
2020-09-08 00:17:37 +0800

    add files & folders

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+    <input type="text" class="todo-input" placeholder="What needs to be done">
+    Todo List goes here
+</div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
+<style>
+  .todo-input {
+      width: 100%;
+      padding: 10px 10px;
+      font-size: 18px;
+      margin-bottom: 10px;
+  }
+
+    input:focus {
+        outline: 0;
+    }
+
+</style>
