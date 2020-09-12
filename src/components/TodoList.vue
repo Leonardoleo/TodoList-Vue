55aba7f3d891a9f65d3480373c65b2811518ceb1
Mo Leo Sam <leo89_my@hotmail.com>
2020-09-12 13:53:45 +0800

    add remove-item

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,8 +1,13 @@
 <template>
 <div>
     <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo" @keyup.enter="addTodo">
-    <div v-for="todo in todos" :key="todo.id" class="todo-item">
-    {{ todo.title }}
+    <div v-for="(todo, index) in todos" :key="todo.id" class="todo-item">
+      <div>
+        {{ todo.title }}
+      </div>
+      <div class="remove-item" @click="removeTodo(index)">
+      &times;
+      </div>
     </div>
 </div>
 </template>
@@ -45,6 +50,10 @@ export default {
 
        this.newTodo = ''
        this.idForTodo++
+     },
+
+     removeTodo(index) {
+       this.todos.splice(index, 1)
      }
   }
 }
@@ -54,13 +63,29 @@ export default {
 <style>
   .todo-input {
       width: 100%;
-      padding: 10px 10px;
+      padding: 10px 18px;
       font-size: 18px;
-      margin-bottom: 10px;
+      margin-bottom: 16px;
   }
 
     input:focus {
         outline: 0;
     }
+
+    .todo-item {
+      margin-bottom: 12px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+       .remove-item {
+      cursor: pointer;
+      margin-left: 14px;
+    }
+       
+       input:hover {
+       color: black;
+     }
 
 </style>
